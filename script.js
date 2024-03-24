1f2ba90fb80c76dd3b5176456496a2677d8f61a4
jazzocordeiro <cordeirorosadigital@gmail.com>
2024-03-24 00:00:00 -0300

    Seu comentário de commit aqui

---
+++ script.js
@@ -0,0 +1,13 @@
+/*##############################################################################################################
+                           VARIÁVEIS
+Variáveis são informações que PODEM MUDAR OU RECEBER NOVOS VALORES de acordo com o input ou circunstancias do próprio código.
+*/
+
+const { setImmediate } = require("timers/promises")
+
+console.log("digite o nome do seu jogador.")
+
+let nomeJogador = "jazao"
+
+console.log("Bem vindo " + nomeJogador)
+
