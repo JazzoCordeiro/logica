b5b45b18a14c07620cfa226b828b02c10f44657c
jazzocordeiro <cordeirorosadigital@gmail.com>
2024-03-25 00:00:00 -0300

    Variáveis

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ let nomeJogador = "jazao"
 
 console.log("Bem vindo " + nomeJogador)
 
+
+/* ###############################################################################################################
+                            CONSTANTES
+Constante sãos informações que NÃO PODEM MUDAR E NEM RECEBER NOVOS VALORES e podem ser chamadas a qualquer instante.
+*/
+
+const notificacao = "Poke go diz:"
+
+console.log(notificacao + "Tem um novo Pokemon na região.")
+
+console.log(notificacao + "Você foi derrotado.")
+
+/*################################################################################################################
+                            EXERCÍCIOS
+        Assim como na matemática, constantes são valores imutáveis, que não dependem de contexto e variáveis são valores que 
+    vão depender e mudar de acordo com as condições ou da equação.
+        Portanto, a principal diferença é que uma constante tem um valor fixo e imutável, enquanto uma variável pode assumir 
+    diferentes valores.
+        Quando atribuo NOVO VALOR a uma variável, não é preciso declarar ela usando LET.
+
+        Há também o VAR: Quando declarado fora de uma função, o escopo é global, mas quando declarado dentro de uma função
+    o escopo é local, de resto ela permite redeclaração e reatribuição.
+*/
+
+const mensagem = "Uma constante não pode ser alterada depois de declarada, seu valor não muda, porém "
+
+let varia = "uma variável pode ser alterada depois de declarada"
+
+console.log(mensagem + varia)
+
+varia = "a variável pode ser alterada de acordo com a necessidade do programa e ao atribuir um novo valor, não preciso declarar ela novamente usando o LET, uso apenas seu nome"
+
+console.log(mensagem + varia)
+
