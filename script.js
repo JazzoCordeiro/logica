07e58cee60b1efee5f7019103bc8f3c4d6fcb288
jazzocordeiro <cordeirorosadigital@gmail.com>
2024-04-13 08:12:50 -0300

    Desafio Felipão

--- script.js
+++ script.js
@@ -313,3 +313,24 @@ while (passos < 6) {
 
 /*Enquanto o FOR vai repetir uma condição e adicionar isso à variável, o WHILE vai repetir a variável até chegar na 
 condição. O DO WHILE vai executar ao menos uma vez o que for de seu escopo, enquanto o while não.*/
+
+
+//DESAFIO DO FELIPÃO
+
+let heroi = "Domenico Danubio"
+let exp = 5000
+
+Se XP for menor do que 1.000 = Ferro
+Se XP for entre 1.001 e 2.000 = Bronze
+Se XP for entre 2.001 e 5.000 = Prata
+Se XP for entre 5.001 e 7.000 = Ouro
+Se XP for entre 7.001 e 8.000 = Platina
+Se XP for entre 8.001 e 9.000 = Ascendente
+Se XP for entre 9.001 e 10.000= Imortal
+Se XP for maior ou igual a 10.001 = Radiante
+
+
+switch (rankHeroi){
+    case (exp < 1000):
+        alert(`O herói ${heroi} é de rank ferro`)
+    break}
