ef1249ffd77a703a74f21361da608af219b0986b
jazzocordeiro <cordeirorosadigital@gmail.com>
2024-06-13 07:45:59 -0300

    estudos

--- script.js
+++ script.js
@@ -589,7 +589,7 @@ num.sort()
 console.log("E depois de usar o sort ele ficou na seguinte ordem " + num)
 
 for(let pos=0; pos<num.length; pos++){
-    console.log(num[pos])
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
 
 
