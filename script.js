5f0c9ad0c8ebb4eaf88836546ab1e3cbabaa02b2
jazzocordeiro <cordeirorosadigital@gmail.com>
2024-06-14 08:45:59 -0300

    exercicio.

--- script.js
+++ script.js
@@ -592,7 +592,9 @@ for(let pos=0; pos<num.length; pos++){
     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
 
-
+for(let pos in num) {
+    console.log("Agora usando o FOR IN que é para objetos e vetores/arrays " + num[pos])
+}
 
 
 
