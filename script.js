b2166b20ff393bc751203179ee5d422082568a9e
jazzocordeiro <cordeirorosadigital@gmail.com>
2024-07-08 09:45:59 -0300

    estudos

--- script.js
+++ script.js
@@ -742,5 +742,7 @@ de outra.
 
 
 /*#########################################################################################################################
-
+A grosso modo, um objeto é basicamente uma variavel com muitos valores dentro, mas, ao invés de serem armazenados de 
+forma sequencial como em um array, os valores são armazenados com chaves (ou propriedades) que permitem identificar e 
+acessar cada valor.
 */
