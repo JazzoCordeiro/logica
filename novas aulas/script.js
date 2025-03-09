50269d2351bf83e662df0f61ed3d3fc5b7fd13ab
jazzocordeiro <cordeirorosadigital@gmail.com>
2025-03-09 14:30:00 -0300

    revisões

--- novas aulas/script.js
+++ novas aulas/script.js
@@ -1,3 +1,10 @@
 /* MODO STRICT do javascript O "use strict" é uma diretiva que ativa um modo de execução mais restrito do código. Ele 
 foi introduzido no ECMAScript 5 (ES5) com o objetivo de tornar o JavaScript mais seguro, mais previsível e menos 
 propenso a erros.  */
+
+
+/* && -> E
+   || -> OU
+   ! -> Negação
+   !! -> negação da negação
+   */
